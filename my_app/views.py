ef38673ed16e0ab71f921da000d2a73357f2c23a
Taher Irfan <taherhphs@gmail.com>
2020-05-12 02:13:05 +0600

    front end

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import request
 
 
 # Create your views here.
 def home(request):
-    print("here")
+
     return render(request, template_name='base.html')
+
+
+def new_search(request):
+
+    search = request.POST.get('search')
+    context = {
+        'search': search
+    }
+
+    return render(request, template_name='my_app/new_search.html', context=context)
